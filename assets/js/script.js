db4422c1a31cf95a1b73bc7871a297e83704cf69
Yvan Kalalo <kalaloyvan07@gmail.com>
2025-10-31 21:13:18 +0800

    My Second Commit(with 'X' icon)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
 function toggleMenu() {
     const nav = document.getElementById('navLinks');
+    const icon = document.getElementById('menuIcon');
+    
     nav.classList.toggle('active');
+    
+    if (nav.classList.contains('active')) {
+        icon.textContent = '✖';
+    } else {
+        icon.textContent = '☰';
+    }
 }
